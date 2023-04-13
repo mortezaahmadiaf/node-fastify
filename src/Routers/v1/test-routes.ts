01cd2ceb98070200fd89c328a92cb49cad0a2e52
mortezaahmadiaf <mortezaahmadi.af@gmail.com>
2023-04-13 11:43:37 +0430

    add api to generate and check jwt api

--- src/Routers/v1/test-routes.ts
+++ src/Routers/v1/test-routes.ts
@@ -1,6 +1,8 @@
-import { FastifyInstance } from "fastify";
+import { FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
 import { BaseRouter } from "../../Features/Utilities";
 import { TestController } from "../../Controllers/v1";
+import { authJwt } from "../../Features/Middlewares";
+import { AccessPolicy } from "../../Features/Policies";
 export class TestRouter extends BaseRouter {
   constructor(fastify: FastifyInstance, opts: any, done: any) {
     super(TestController, fastify, opts, done);
@@ -8,9 +10,28 @@ export class TestRouter extends BaseRouter {
   }
 
   init = () => {
-    super.init();
-    this.router.get("/check", (req, res) => {
-      res.send({ morteza: "ahmadi " });
-    });
+    const accessPolicy = new AccessPolicy();
+    accessPolicy.get = authJwt;
+    super.init(accessPolicy);
+    this.router.get(
+      "/jwt-generate",
+      (req: FastifyRequest, res: FastifyReply) => {
+        this.generateJWT(req, res);
+      }
+    );
+
+    this.router.post(
+      "/jwt-decorate",
+      (req: FastifyRequest, res: FastifyReply) => {
+        this.decorateJWT(req, res);
+      }
+    );
+  };
+
+  private generateJWT = (req: FastifyRequest, res: FastifyReply) => {
+    this.controller.generateJWT(req, res);
+  };
+  private decorateJWT = (req: FastifyRequest, res: FastifyReply) => {
+    this.controller.decorateJWT(req, res);
   };
 }
